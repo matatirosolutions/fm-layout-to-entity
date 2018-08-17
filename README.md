d2a5bb4cae86d0a7fbbbe4eb60b9d10b4b948292
Steve Winter <steve@msdev.co.uk>
2018-08-17 17:05:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# FileMaker layout to entity generator #
+
+This command will convert a json object to a Doctrine Entity. It's been written to support the work which we do linking Symfony to FileMaker but could be used to convert any correctly structured json to a Doctrine entity (though note it only supports the FileMaker field types which makes it **very** incomplete.)
+
+## Installation ##
+
+```bash
+    mkdir layout-to-entity && cd layout-to-entity
+    git clone git@github.com:matatirosolutions/fm-layout-to-entity.git .
+    composer install
+```
+## Usage ##
+
+```bash
+Description:
+  Convert a JSON representation of a FileMaker layout to a Doctrine entity.
+
+Usage:
+  convert [options] [--] <file> <destination> <entity>
+
+Arguments:
+  file                  Path to the json file exported from FileMaker.
+  destination           Location to save the entity. If you also generate a repo, then they will be put in appropriate sub folders of the location given.
+  entity                The name of the entity to generate.
+
+Options:
+  -r, --repo[=REPO]     Should a repository also be generated. [default: false]
+  -h, --help            Display this help message
+  -q, --quiet           Do not output any message
+  -V, --version         Display this application version
+      --ansi            Force ANSI output
+      --no-ansi         Disable ANSI output
+  -n, --no-interaction  Do not ask any interactive question
+  -v|vv|vvv, --verbose  Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug
+```
+For example
+
+```bash
+    bin/console convert ./fields.json ../MySymfonyProject/src Order -r=true
+```
+## JSON structure ##
+
+Because this is intended to be used with FileMaker layouts, the table name is referred to as the Layout.
+
+```json
+{
+    "layout": "Order",
+    "fields": [
+        {
+            "field": "_kRecordID",
+            "type": "Number"
+        },
+        {
+            "field": "ClientPO",
+            "type": "Text"
+        }
+}
+```
+
+## TODO ##
+
+ - auto-generate the json by calling a FileMaker script through the FileMaker PHP or Data API
+ 
+## Contact ##
+
+See this [blog post](https://msdev.co.uk/fm-layout-to-entity) for more details.
+
+Steve Winter
+Matatiro Solutions
+hello@msdev.co.uk
+@steveWinterNZ
---
+++ src/LayoutToEntity.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stevewinter
+ * Date: 17/08/2018
+ * Time: 15:17
+ */
+
+namespace Console;
+
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Console\Components;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class LayoutToEntity extends SymfonyCommand
+{
+    public function configure()
+    {
+        $this -> setName('convert')
+            -> setDescription('Convert a JSON representation of a FileMaker layout to a Doctrine entity.')
+            -> addArgument('file', InputArgument::REQUIRED, 'Path to the json file exported from FileMaker.')
+            -> addArgument('destination', InputArgument::REQUIRED, 'Location to save the entity. If you also generate a repo, then they will be put in appropriate sub folders of the location given.')
+            -> addArgument('entity', InputArgument::REQUIRED, 'The name of the entity to generate.')
+            -> addOption('repo', 'r', InputOption::VALUE_OPTIONAL, 'Should a repository also be generated.', false);
+    }
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $json = $this->loadFieldsFromFile($input->getArgument('file'), $output);
+        $content = $this->generateHeader($input, $json->layout);
+
+        foreach($json->fields as $field) {
+            $content .= Components::{$field->type}($field->field);
+        }
+        $content .= Components::footer();
+
+        $this->writeToFile($input, $output, $content);
+        $output->writeln('<info>Entity generated</info>');
+    }
+
+
+    /**
+     * @param $file
+     * @param OutputInterface $output
+     *
+     * @return object
+     */
+    private function loadFieldsFromFile($file, OutputInterface $output)
+    {
+        if(!file_exists($file)) {
+            $output->writeln('<error>Unable to find file. Please check the path is correct</error>');
+            exit();
+        }
+
+        return json_decode(
+            file_get_contents($file)
+        );
+    }
+
+    private function generateHeader(InputInterface $input, $layout)
+    {
+        $entity = $input->getArgument('entity');
+
+        return Components::header($entity, $layout, $input->getOption('repo'));
+    }
+
+
+    private function writeToFile(InputInterface $input, OutputInterface $output, string $content)
+    {
+        $entity = $input->getArgument('entity');
+        $path = $this->entityDestination($input);
+
+        if(!is_writable($path)) {
+            $output->writeln(sprintf('<error>Unable to write to destination %s</error>', $path));
+            exit();
+        }
+
+        $file =  $path . $entity .'.php';
+        if(file_exists($file)) {
+            $helper = $this->getHelper('question');
+            $output->writeln('<question>This entity already exists and will be overwritten.</question>');
+            $question = new ConfirmationQuestion('Continue with this action? [y/N]', false);
+
+            if (!$helper->ask($input, $output, $question)) {
+                $output->writeln('<info>Aborted write as file already exists</info>');
+                exit();
+            }
+        }
+
+        file_put_contents($file, $content);
+        if($input->getOption('repo')) {
+            $repo = $this->repoDestination($input);
+            file_put_contents($repo, Components::repo($entity));
+        }
+    }
+
+
+    private function entityDestination(InputInterface $input)
+    {
+        $base = $this->basePath($input);
+        if($input->getOption('repo')) {
+            $base .= 'Entity' . DIRECTORY_SEPARATOR;
+        }
+
+        return $base;
+    }
+
+    private function repoDestination(InputInterface $input)
+    {
+        return $this->basePath($input) . 'Repository' . DIRECTORY_SEPARATOR .
+            $input->getArgument('entity') . 'Repository.php';
+    }
+
+    private function basePath(InputInterface $input)
+    {
+        $base = $input->getArgument('destination');
+        if(DIRECTORY_SEPARATOR != substr($base, -1)) {
+            $base .= DIRECTORY_SEPARATOR;
+        }
+
+        return $base;
+    }
+}
